8121bd7247d517db75a7c66f231a8c05b36ebb4e
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2023-01-28 10:37:28 +0100

    Added the models for the database

---
+++ models/Comment.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+    comment : {
+        type: String,
+        required: true,
+        maxLength: 1000,
+    },
+    user : {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    post : {
+        type: Schema.Types.ObjectId,
+        ref: 'Post',
+        required: true,
+    },
+    createdAt : {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Comment', CommentSchema);
+
