0d52aab6ef42e18624e95ee5a1ccb1ef0ab83227
chidubem100 <dubemernest23@gmail.com>
2023-02-01 22:35:05 +0100

    new: completed error feature

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -10,16 +10,27 @@ const errorHandler = (err,req,res,next) =>{
         msg: err.message || 'Something went wrong, please try again'
     }
 
+    // validation error (check for err.name === validationError)
+    if(err.name === 'ValidationError'){
+        customError.msg = Object.values(err.errors).maps((item) => item.message.join(','));
+        customError.statuscode = 400;
+    }
+
+    // duplicate error (check for err.code === 11000)
+    if(err.code === 11000){
+        customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please pick another value`;
+        customError.statuscode = 400;
+    }
+
+    // cast error (check for err.name === castError)
+    if(err.name === 'CastError'){
+        customError.msg = `No item found with such id`;
+        customError.statuscode = 404;
+    }
+
 
     return res.status(customError.statuscode).json({success: false, msg: customError.msg});
     
 }
-
-// validation error
-// duplicate error
-// cast error
-
-
-
 
 module.exports = errorHandler;
