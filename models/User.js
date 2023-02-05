6601c987ac69793693488807c8482a2f499e407c
chidubem100 <dubemernest23@gmail.com>
2023-02-05 06:45:08 +0100

    new: refactor

--- models/User.js
+++ models/User.js
@@ -1,26 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const validator = require('validator') //email validator
+
 
 const UserSchema = new Schema({
     email : {
         type: String,
         required: true,
         unique: true,
+        minLength: 2,
+        maxLength: 50,
+        validate: {
+            validator: validator.isEmail,
+            message: 'Please provide a valid email'
+        }
     },
     password : {
         type: String,
-        required: true,
+        required: [true, "please provide password"],
         minLength: 7,
     },
     username : {
         type: String,
-        required: true,
+        required: [true, "Please provide a username"],
         unique: true,
+        maxLength: 50,
+        minLength: 2,
     },
     profileInfo : {
         type: String,
-        default: '',
-        maxLength: 500,
+        default: 'my profile bio',
+        maxLength: [500, "Profile info should not exceed 500 characters"],
     },
     role : {
         type: String,
