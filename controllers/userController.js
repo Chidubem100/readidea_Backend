ae6ef8b4a72a74db741b55ae7d42d6fa93dc9cf6
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2023-03-30 18:31:50 +0100

    added password reset token and functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,9 @@
 const User = require('../models/User');
-const {attachCookiesToResponse} = require('../utils');
+const {attachCookiesToResponse,
+        sendEmail} = require('../utils');
 const {StatusCodes} = require('http-status-codes');
 const CustomApiError = require('../errors');
+const crypto = require('crypto');
 
 // getAllUsers
 const getAllUsers = async(req,res) =>{
@@ -75,11 +77,74 @@ const showCurrentUser = async(req,res) =>{
     res.status(StatusCodes.OK).json({ user:req.user })
 };
 
+// reset password token
+const token = async(req,res) =>{
+    const {email} = req.body;
+    if(!email){
+        throw new CustomApiError.BadRequestError('Please provide the needed values')
+    }
+
+    const user = await User.findOne({email});
+
+    if(!user){
+        throw new CustomApiError.BadRequestError('No user with email ' + email);
+    }
+
+    const token = await user.createPasswordResetToken();
+    await user.save();
+
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/user/resetPassword/${token}`;
+
+    const message = {
+        to: user.email,
+        subject: 'Password reset token',
+        html: `<p>Please click on the link below to reset your password</p> <a href="${resetUrl}">Reset Password</a>`
+    }
+
+    await sendEmail(message);
+
+    res.status(StatusCodes.OK).json({msg: 'Token sent to ' + user.email, token});
+};
+
+// reset password
+const resetPassword = async(req,res) =>{
+    const {
+        body: { password },
+        params: { token },
+    } = req;
+
+    if(!password){
+        throw new CustomApiError.BadRequestError('Please provide the needed values')
+    }
+
+    const hashedToken = crypto.createHash('sha256').update(token).digest('hex');
+
+    const user = await User.findOne({passwordResetToken: hashedToken, passwordResetExpires: {$gt: Date.now()}});
+
+    if(!user){
+        throw new CustomApiError.BadRequestError('Token is invalid or has expired')
+    }
+
+    user.password = password;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save();
+
+    res.status(StatusCodes.OK).json({msg: 'Password updated successfully'});
+};
+
+
+
+
+
+
 
 module.exports = {
     getAllUsers,
     getSingleUser,
     updateProfile,
     changePassword,
-    showCurrentUser
+    showCurrentUser,
+    token,
+    resetPassword
 };
