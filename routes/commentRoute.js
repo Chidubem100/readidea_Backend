4376824dc9094b0b8de52350fadac640a2066b2b
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2023-04-11 23:29:09 +0100

    Add replies to comment

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -6,12 +6,18 @@ const {
     createComment,
     getSingleComment,
     getAllComments,
-    deleteComment 
+    deleteComment,
+    addReply,
+    deleteReply
 } = require('../controllers/commentController');
 
 router.post('/', authenticate, createComment);
 router.get('/:postId', authenticate, getAllComments);
 router.delete('/:commentId', authenticate, deleteComment);
 router.get('/single/:commentId', authenticate, getSingleComment);
+router.post('/reply', authenticate, addReply);
+router.delete('/reply/:replyId', authenticate, deleteReply);
+
+
 
 module.exports = router;
